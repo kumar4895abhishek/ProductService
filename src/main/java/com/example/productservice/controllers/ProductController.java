e9a4a93d82fa2bcfc81d1703e645c214996b79b3
Abhishek Kumar <31275711+kumar4895abhishek@users.noreply.github.com>
2023-11-23 08:26:34 +0530

    ProductService getProductsById and getAllProducts

---
+++ src/main/java/com/example/productservice/controllers/ProductController.java
@@ -0,0 +1,2 @@
+package com.example.productservice.controllers;public class ProductController {
+}
---
+++ src/main/java/com/example/productservice/services/FakeStoreProductService.java
@@ -0,0 +1,2 @@
+package com.example.productservice.services;public class FakeStoreProductService {
+}
