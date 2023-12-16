3d182250d31f1444b1537d014a69fbbed494dfaf
Abhishek Kumar <31275711+kumar4895abhishek@users.noreply.github.com>
2023-12-16 08:36:44 +0530

    createProductShouldCreateAValidNewProduct  for WebMVC test

--- src/test/java/com/example/productservice/controllersTest/ProductControllerTest.java
+++ src/test/java/com/example/productservice/controllersTest/ProductControllerTest.java
@@ -7,15 +7,19 @@ import com.example.productservice.exceptions.ProductNotFoundException;
 import com.example.productservice.services.ProductService;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
+import org.mockito.Captor;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import org.mockito.ArgumentCaptor;
 
 @SpringBootTest
 public class ProductControllerTest
@@ -25,6 +29,9 @@ public class ProductControllerTest
 
     @Inject
     private ProductController productController;
+
+    @Captor
+    private ArgumentCaptor<Long> argumentCaptor;
 
 
     @Test
@@ -54,6 +61,23 @@ public class ProductControllerTest
         GenericProductDto genericProductDto1=productController.getProductById(10L);
 
         assertEquals(genericProductDto,genericProductDto1);
+
+    }
+
+    @Test
+    @DisplayName("testProductControllerCallsProductServiceWithSameProductIdAsInput")
+    void testIfSameInput() throws ProductNotFoundException {
+
+        Long id = 100L;
+
+        when(productService.getProductById(id)).thenReturn(new GenericProductDto());
+
+        GenericProductDto genericProductDto=productController.getProductById(id);
+
+        verify(productService).getProductById(argumentCaptor.capture());
+
+        assertEquals(id, argumentCaptor.getValue());
+
 
     }
 
--- src/test/java/com/example/productservice/controllersTest/ProductControllerWebMvcTest.java
+++ src/test/java/com/example/productservice/controllersTest/ProductControllerWebMvcTest.java
@@ -2,21 +2,26 @@ package com.example.productservice.controllersTest;
 
 
 import com.example.productservice.controllers.ProductController;
+import com.example.productservice.dtos.GenericProductDto;
 import com.example.productservice.services.ProductService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @WebMvcTest(ProductController.class)
 public class ProductControllerWebMvcTest
@@ -44,4 +49,49 @@ public class ProductControllerWebMvcTest
                 .andExpect(content().string("[]"));
     }
 
+    @Test
+    void testGetAllProductsReturnsValidList() throws Exception {
+        List<GenericProductDto> genericProductDtos = new ArrayList<>();
+        genericProductDtos.add(new GenericProductDto());
+        genericProductDtos.add(new GenericProductDto());
+        genericProductDtos.add(new GenericProductDto());
+
+        when(productService.getAllProducts())
+                .thenReturn(genericProductDtos);
+
+        mockMvc.perform(get("/products"))
+                .andExpect(status().is(200))
+                .andExpect(content().string(objectMapper.writeValueAsString(genericProductDtos)));
+    }
+
+    @Test
+    void createProductShouldCreateAValidNewProduct() throws Exception {
+        GenericProductDto productToCreateDto = new GenericProductDto();
+        productToCreateDto.setTitle("Macbook");
+        productToCreateDto.setPrice(200000);
+        productToCreateDto.setDescription("Fastest mac ever");
+        productToCreateDto.setCategory("Laptop");
+
+        GenericProductDto outputGenericProductDto = new GenericProductDto();
+        outputGenericProductDto.setCategory(productToCreateDto.getCategory());
+        outputGenericProductDto.setTitle(productToCreateDto.getTitle());
+        outputGenericProductDto.setPrice(productToCreateDto.getPrice());
+        outputGenericProductDto.setDescription(productToCreateDto.getDescription());
+        outputGenericProductDto.setId(1000L);
+
+        when(productService.createProduct(any()))
+                .thenReturn(outputGenericProductDto);
+
+        mockMvc.perform(post("/products")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(productToCreateDto))
+                )
+                .andExpect(
+                        content().string(objectMapper.writeValueAsString(outputGenericProductDto))
+                )
+                .andExpect(status().is(200));
+    }
+
 }
+
+
