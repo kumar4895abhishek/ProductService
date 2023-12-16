d01a1aca9b43b6a33d0252b80c95150a38350569
Abhishek Kumar <31275711+kumar4895abhishek@users.noreply.github.com>
2023-12-16 08:45:29 +0530

    createProductShouldCreateAValidNewProduct  JSON path value check test

--- src/test/java/com/example/productservice/controllersTest/ProductControllerWebMvcTest.java
+++ src/test/java/com/example/productservice/controllersTest/ProductControllerWebMvcTest.java
@@ -14,13 +14,14 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
 import java.util.List;
+import static org.hamcrest.Matchers.is;
+
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @WebMvcTest(ProductController.class)
@@ -89,7 +90,8 @@ public class ProductControllerWebMvcTest
                 .andExpect(
                         content().string(objectMapper.writeValueAsString(outputGenericProductDto))
                 )
-                .andExpect(status().is(200));
+                .andExpect(jsonPath("$.title",is("Macbook")))
+                .andExpect(jsonPath("$.price",is(200000)));
     }
 
 }
